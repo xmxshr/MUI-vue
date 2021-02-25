ac68404061d5e10f57fb41f6c8c36eebc8191594
xumx <xumanxi626@hotmail.com>
2021-02-25 17:23:22 +0800

    feat: 完成Button组件基础样式

--- src/components/ButtonDoc.vue
+++ src/components/ButtonDoc.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="sub-page">
-    button 组件文档
-  </div>
+  <h1>Button 组件</h1>
+  <h2>示例1</h2>
+  <Button>你好</Button>
 </template>
 
+<script lang="ts">
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.sub-page {
-  height: 150vh;
-}
 </style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="m-button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+$min-width: 60px;
+$height: 32px;
+$theme-color: #40a9ff;
+$color: rgba(0, 0, 0, .85);
+$border-color: #d9d9d9;
+$radius: 2px;
+
+.m-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  min-width: $min-width;
+  height: $height;
+  padding: 0 12px;
+  font-size: 14px;
+  white-space: nowrap;
+  color: $color;
+  background-color: #fff;
+  box-shadow: 0 1px 0 fade-out($color: #000, $amount: 0.95);
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-sizing: border-box;
+  cursor: pointer;
+  outline: none;
+  transition: all 0.25s;
+
+  & + & {
+    margin-left: 10px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $theme-color;
+  }
+}
+</style>
