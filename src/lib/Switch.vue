066038a543f9440268a75c0958d5ef1b776b93e5
xumx <xumanxi626@hotmail.com>
2021-02-25 14:48:01 +0800

    feat: 完成Switch组件逻辑

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,23 @@
 <template>
-  <button> <span></span> </button>
+  <button :class="{checked: value}" @click="toggleStatus"> <span></span> </button>
 </template>
+
+<script lang="ts">
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggleStatus = () => {
+      context.emit('update:value', !props.value);
+    };
+
+    return {
+      toggleStatus,
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 $gap: 2px;
@@ -11,18 +28,28 @@ button {
   position: relative;
   width: $h * 2;
   height: $h;
-  background-color: #00f;
+  background-color: #666;
   border: none;
   border-radius: $h / 2;
-}
+  outline: none;
 
-span {
-  position: absolute;
-  top: $gap;
-  left: $gap;
-  width: $h2;
-  height: $h2;
-  background-color: #fff;
-  border-radius: $h2 / 2;
+  > span {
+    position: absolute;
+    top: $gap;
+    left: $gap;
+    width: $h2;
+    height: $h2;
+    background-color: #fff;
+    border-radius: $h2 / 2;
+    transition: left 0.25s;
+  }
+
+  &.checked {
+    background-color: #00f;
+
+    > span {
+      left: calc(100% - #{$h2} - #{$gap});
+    }
+  }
 }
 </style>
