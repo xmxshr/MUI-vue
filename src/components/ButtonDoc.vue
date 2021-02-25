67d8eff24dcdeaeafb7ee7e1687e0d0eb3addf10
xumx <xumanxi626@hotmail.com>
2021-02-25 23:04:02 +0800

    feat: 基本完成Button组件

--- src/components/ButtonDoc.vue
+++ src/components/ButtonDoc.vue
@@ -1,7 +1,71 @@
 <template>
   <h1>Button 组件</h1>
-  <h2>示例1</h2>
-  <Button>你好</Button>
+  <h2>示例1 按钮类型</h2>
+  <div>
+    <Button theme="primary">主要primary</Button>
+    <Button>普通normal</Button>
+    <Button theme="link">链接link</Button>
+    <Button theme="text">文字text</Button>
+  </div>
+
+  <h2>示例2 按钮尺寸</h2>
+  <div>
+    <Button theme="primary" size="big">主要primary 大</Button>
+    <Button theme="primary">主要primary 标准</Button>
+    <Button theme="primary" size="small">主要primary 小</Button>
+    <hr />
+    <Button size="big">普通normal 大</Button>
+    <Button>普通normal 标准</Button>
+    <Button size="small">普通normal 小</Button>
+    <hr />
+    <Button theme="link" size="big">链接link 大</Button>
+    <Button theme="link">链接link 标准</Button>
+    <Button theme="link" size="small">链接link 小</Button>
+    <hr />
+    <Button theme="text" size="big">文字text 大</Button>
+    <Button theme="text">文字text 标准</Button>
+    <Button theme="text" size="small">文字text 小</Button>
+  </div>
+
+  <h2>示例3 危险状态</h2>
+  <div>
+    <Button theme="primary" danger>主要primary</Button>
+    <Button danger>普通normal</Button>
+    <Button theme="link" danger>链接link</Button>
+    <Button theme="text" danger>文字text</Button>
+  </div>
+
+  <h2>示例4 禁用状态</h2>
+  <div>
+    <Button theme="primary">主要primary</Button>
+    <Button theme="primary" disabled>主要primary</Button>
+    <hr />
+    <Button>普通normal</Button>
+    <Button disabled>普通normal</Button>
+    <hr />
+    <Button theme="link">链接link</Button>
+    <Button theme="link" disabled>链接link</Button>
+    <hr />
+    <Button theme="text">文字text</Button>
+    <Button theme="text" disabled>文字text</Button>
+    <hr />
+    <Button theme="primary" danger>主要primary</Button>
+    <Button theme="primary" danger disabled>主要primary</Button>
+    <hr />
+    <Button danger>普通normal</Button>
+    <Button danger disabled>普通normal</Button>
+    <hr />
+    <Button theme="link" danger>链接link</Button>
+    <Button theme="link" danger disabled>链接link</Button>
+  </div>
+
+  <h2>示例5 加载中状态</h2>
+  <div>
+    <Button theme="primary" loading>主要primary</Button>
+    <Button loading>普通normal</Button>
+    <Button theme="link" loading>链接link</Button>
+    <Button theme="text" loading>文字text</Button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,4 +79,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+div:last-child {
+  padding-bottom: 100px;
+}
+button + button {
+  margin-left: 10px;
+}
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,13 +1,60 @@
 <template>
-  <button class="mui-button">
+  <button
+    class="mui-button"
+    :class="classes"
+    :disabled="disabled"
+  >
+    <span v-if="loading" class="mui-btn-loading-icon"></span>
     <slot />
   </button>
 </template>
+
+<script lang="ts">
+import { computed } from 'vue';
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'default',
+    },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    danger: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const { theme, size, danger } = props;
+    const classes = computed(() => {
+      return {
+        [`mui-btn-theme-${theme}`]: theme,
+        [`mui-btn-size-${size}`]: size,
+        'mui-btn-danger': danger,
+      };
+    });
+    return {
+      classes,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 $min-width: 60px;
 $height: 32px;
 $theme-color: #40a9ff;
+$warn-color: #ff4d4f;
 $color: rgba(0, 0, 0, .85);
 $border-color: #d9d9d9;
 $radius: 2px;
@@ -31,13 +78,138 @@ $radius: 2px;
   outline: none;
   transition: all 0.25s;
 
-  & + & {
-    margin-left: 10px;
-  }
-
   &:hover,
   &:focus {
     color: $theme-color;
+  }
+
+  &.mui-btn-theme-primary {
+    color: #fff;
+    background-color: $theme-color;
+    border-color: $theme-color;
+
+    &:hover,
+    &:focus {
+      background-color: darken($theme-color, 10%);
+    }
+  }
+
+  &.mui-btn-theme-link {
+    color: $theme-color;
+    border-color: transparent;
+    box-shadow: none;
+
+    &:hover,
+    &:focus {
+      color: darken($theme-color, 10%);
+    }
+  }
+
+  &.mui-btn-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+
+    &:hover,
+    &:focus {
+      color: $color;
+      background-color: #f9f9f9;
+    }
+  }
+
+  &.mui-btn-size-big {
+    height: 40px;
+    font-size: 16px;
+  }
+
+  &.mui-btn-size-small {
+    height: 24px;
+  }
+
+  &.mui-btn-danger {
+    color: $warn-color;
+    background-color: #fff;
+    border-color: $warn-color;
+
+    &:hover,
+    &:focus {
+      color: darken($warn-color, 10%);
+    }
+
+    &.mui-btn-theme-primary {
+      color: #fff;
+      background-color: $warn-color;
+      border-color: $warn-color;
+
+      &:hover,
+      &:focus {
+        background-color: darken($warn-color, 10%);
+      }
+    }
+
+    &.mui-btn-theme-link,
+    &.mui-btn-theme-text {
+      color: $warn-color;
+      background-color: transparent;
+      border-color: transparent;
+
+      &:hover,
+      &:focus {
+        color: darken($warn-color, 10%);
+      }
+    }
+  }
+
+
+  &[disabled],
+  &.mui-btn-theme-primary[disabled] {
+    cursor: not-allowed;
+
+    &,
+    &:active,
+    &:focus,
+    &:hover {
+      color: rgba(0, 0, 0, .25);
+      background-color: #f5f5f5;
+      border-color: #d9d9d9;
+      box-shadow: none;
+    }
+  }
+
+  &.mui-btn-theme-link[disabled],
+  &.mui-btn-theme-text[disabled] {
+    cursor: not-allowed;
+
+    &,
+    &:active,
+    &:focus,
+    &:hover {
+      color: rgba(0, 0, 0, .25);
+      background-color: transparent;
+      border-color: transparent;
+      box-shadow: none;
+    }
+  }
+
+  .mui-btn-loading-icon {
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    margin-right: 6px;
+    border: 1px solid;
+    border-top-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    border-radius: 10px;
+    animation: mui-circle 1s linear infinite;
+  }
+}
+
+@keyframes mui-circle {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
