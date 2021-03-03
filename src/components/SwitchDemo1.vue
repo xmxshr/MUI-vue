3dc1945c8cc168b55b234c9b0d2afde1f643a76e
xumx <xumanxi626@hotmail.com>
2021-03-03 19:38:23 +0800

    feat: 添加源码转换

---
+++ src/components/SwitchDemo1.vue
@@ -0,0 +1,24 @@
+<demo>基础用法</demo>
+<desc>按钮的基础用法</desc>
+
+<template>
+  <Switch v-model:value="bool" />
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import Switch from '../lib/Switch.vue';
+
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    // 设置初始值
+    const bool = ref(false);
+    return {
+      bool,
+    };
+  },
+};
+</script>
--- src/views/doc/SwitchDoc.vue
+++ src/views/doc/SwitchDoc.vue
@@ -1,25 +1,65 @@
 <template>
-  <div>
-    <Switch v-model:value="bool" />
+  <div class="demo">
+    <h3 class="demo-title">{{SwitchDemo1.__sourceCodeTitle}}</h3>
+    <p class="demo-desc">{{SwitchDemo1.__sourceCodeDesc}}</p>
+    <div class="demo-content">
+      <div class="demo-comp">
+        <SwitchDemo1 />
+      </div>
+      <div class="demo-button">
+        <Button>查看源码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{SwitchDemo1.__sourceCode}}</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-import Switch from '../../lib/Switch.vue';
+import SwitchDemo1 from '../../components/SwitchDemo1.vue';
+import Button from '../../lib/Button.vue';
 
 export default {
   components: {
-    Switch,
+    Button,
+    SwitchDemo1,
   },
   setup() {
-    const bool = ref(false);
     return {
-      bool,
+      SwitchDemo1,
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+$border-color: #ccc;
+
+.demo {
+  .demo-title {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  .demo-content {
+    border: 1px solid $border-color;
+    border-radius: 4px;
+  }
+
+  .demo-comp {
+    padding: 10px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  .demo-button {
+    padding: 10px;
+    text-align: center;
+  }
+
+  .demo-code {
+    padding: 10px;
+    border-top: 1px solid $border-color;
+  }
+}
 </style>
